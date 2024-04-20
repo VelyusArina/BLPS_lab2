a7265598aab122db17ab908d26f4377fd098fe18
artemiy <Ntvrfntvrf20031905!>
2024-04-21 00:10:30 +0300

    add: country enum

---
+++ src/main/java/ru/artemiyandarina/blps_lab2/models/Country.java
@@ -0,0 +1,15 @@
+package ru.artemiyandarina.blps_lab2.models;
+
+public enum Country {
+    USA,
+    RUSSIA,
+    UK,
+    GERMANY,
+    FRANCE,
+    CANADA,
+    AUSTRALIA,
+    JAPAN,
+    CHINA,
+    INDIA,
+    BRAZIL;
+}
