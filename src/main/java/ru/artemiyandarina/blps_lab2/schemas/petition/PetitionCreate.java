ed5658f4ec61565b76c754691ebc44ea6fa9a080
artemiy <Ntvrfntvrf20031905!>
2024-04-22 01:08:33 +0300

    fix: examples

--- src/main/java/ru/artemiyandarina/blps_lab2/schemas/petition/PetitionCreate.java
+++ src/main/java/ru/artemiyandarina/blps_lab2/schemas/petition/PetitionCreate.java
@@ -1,5 +1,7 @@
 package ru.artemiyandarina.blps_lab2.schemas.petition;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,6 +10,8 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class PetitionCreate extends PetitionBase {
-    @Size(min = 3, max = 30)
+    @Schema(example = "RUSSIA")
+    @NotBlank
+    @Size(max = 20)
     String country;
 }
