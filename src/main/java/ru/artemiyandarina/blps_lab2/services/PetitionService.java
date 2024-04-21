998b6d211313384e4d64bee5ec74dd9f429c5669
artemiy <Ntvrfntvrf20031905!>
2024-04-22 01:09:56 +0300

    add: software transaction processing

--- src/main/java/ru/artemiyandarina/blps_lab2/services/PetitionService.java
+++ src/main/java/ru/artemiyandarina/blps_lab2/services/PetitionService.java
@@ -1,39 +1,53 @@
 package ru.artemiyandarina.blps_lab2.services;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import ru.artemiyandarina.blps_lab2.exceptions.NotFoundException;
 import ru.artemiyandarina.blps_lab2.models.Petition;
 import ru.artemiyandarina.blps_lab2.repositories.PetitionRepository;
 import ru.artemiyandarina.blps_lab2.schemas.petition.PetitionCreate;
 import ru.artemiyandarina.blps_lab2.schemas.petition.PetitionRead;
 import ru.artemiyandarina.blps_lab2.services.mapping.PetitionMapper;
-import ru.artemiyandarina.blps_lab2.exceptions.NotFoundException;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class PetitionService {
     final PetitionRepository petitionRepository;
     final PetitionMapper petitionMapper;
     final SecurityService securityService;
+    final PlatformTransactionManager transactionManager;
+
+    @Autowired
+    public PetitionService(PetitionRepository petitionRepository, PetitionMapper petitionMapper, SecurityService securityService, PlatformTransactionManager transactionManager) {
+        this.petitionRepository = petitionRepository;
+        this.petitionMapper = petitionMapper;
+        this.securityService = securityService;
+        this.transactionManager = transactionManager;
+    }
 
     public Set<PetitionRead> getAll() {
-        return petitionRepository.findAll().stream()
-                .map(petitionMapper::mapEntityToPetitionRead)
-                .collect(Collectors.toSet());
+        return petitionRepository.findAll().stream().map(petitionMapper::mapEntityToPetitionRead).collect(Collectors.toSet());
     }
 
-    @Transactional
     public PetitionRead create(PetitionCreate schema) {
-        Petition newPetition = petitionMapper.mapPetitionCreateToEntity(schema);
-        newPetition.setOwner(securityService.getCurrentUser());
-        petitionRepository.save(newPetition);
-        return petitionMapper.mapEntityToPetitionRead(newPetition);
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        TransactionStatus status = transactionManager.getTransaction(def);
+        try {
+            Petition newPetition = petitionMapper.mapPetitionCreateToEntity(schema);
+            newPetition.setOwner(securityService.getCurrentUser());
+            petitionRepository.save(newPetition);
+            transactionManager.commit(status);
+            return petitionMapper.mapEntityToPetitionRead(newPetition);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
-
 
     public PetitionRead getById(Long id) {
         Petition petition = petitionRepository.findById(id).orElseThrow(NotFoundException::new);
@@ -46,3 +60,4 @@ public class PetitionService {
         petitionRepository.delete(petition);
     }
 }
+
