b25eb3ed35fd0b1686138a7fc2651336573b0b5e
artemiy <Ntvrfntvrf20031905!>
2024-04-22 01:09:22 +0300

    add: WrongCountryNameException

---
+++ src/main/java/ru/artemiyandarina/blps_lab2/exceptions/WrongCountryNameException.java
@@ -0,0 +1,4 @@
+package ru.artemiyandarina.blps_lab2.exceptions;
+
+public class WrongCountryNameException extends IllegalArgumentException {
+}
--- src/main/java/ru/artemiyandarina/blps_lab2/services/mapping/PetitionMapper.java
+++ src/main/java/ru/artemiyandarina/blps_lab2/services/mapping/PetitionMapper.java
@@ -1,11 +1,13 @@
 package ru.artemiyandarina.blps_lab2.services.mapping;
 
 import org.springframework.stereotype.Service;
+import ru.artemiyandarina.blps_lab2.exceptions.WrongCountryNameException;
 import ru.artemiyandarina.blps_lab2.models.Country;
 import ru.artemiyandarina.blps_lab2.models.Petition;
 import ru.artemiyandarina.blps_lab2.schemas.petition.PetitionBase;
 import ru.artemiyandarina.blps_lab2.schemas.petition.PetitionCreate;
 import ru.artemiyandarina.blps_lab2.schemas.petition.PetitionRead;
+
 @Service
 public class PetitionMapper {
     private final UserMapper userMapper;
@@ -24,19 +26,23 @@ public class PetitionMapper {
         return mapPetitionBaseToEntity(schema, new Petition());
     }
 
+
+    // Вот тут используется транзакция
     public Petition mapPetitionCreateToEntity(PetitionCreate schema) {
         Petition newPetition = mapPetitionBaseToEntity(schema);
-        newPetition.setCountry(Country.valueOf(schema.getCountry()));
-// todo: почему blya страна не сетится.....
+        try {
+            newPetition.setCountry(Country.valueOf(schema.getCountry()));
+        } catch (IllegalArgumentException e) {
+            throw new WrongCountryNameException();
+        }
+        // todo: почему blya страна не сетится.....
         return newPetition;
     }
 
     public PetitionRead mapEntityToPetitionRead(Petition entity) {
         PetitionRead schema = new PetitionRead();
         schema.setId(entity.getId());
-        schema.setOwner(
-                userMapper.mapEntityToUserRead(entity.getOwner())
-        );
+        schema.setOwner(userMapper.mapEntityToUserRead(entity.getOwner()));
         schema.setTitle(entity.getTitle());
         schema.setDescription(entity.getDescription());
         schema.setCreationDate(entity.getCreationDate());
