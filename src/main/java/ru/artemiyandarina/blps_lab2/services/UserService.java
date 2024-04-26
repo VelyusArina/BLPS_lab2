3f0027b2f72128f6832b04ac7acc64dfe41d0d43
artemiy <Ntvrfntvrf20031905!>
2024-04-26 19:30:33 +0300

    fix: now working

--- src/main/java/ru/artemiyandarina/blps_lab2/services/UserService.java
+++ src/main/java/ru/artemiyandarina/blps_lab2/services/UserService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import ru.artemiyandarina.blps_lab2.repositories.UserRepository;
-import ru.artemiyandarina.blps_lab2.repositories.XMLUserRepository;
 import ru.artemiyandarina.blps_lab2.services.mapping.UserMapper;
 import ru.artemiyandarina.blps_lab2.schemas.user.UserRead;
 import ru.artemiyandarina.blps_lab2.schemas.user.UserRegister;
@@ -21,12 +20,10 @@ import ru.artemiyandarina.blps_lab2.exceptions.NotFoundException;
 public class UserService {
     final UserRepository userRepository;
     final UserMapper userMapper;
-    final XMLUserRepository xmlUserRepository;
-
     public UserRead register(UserRegister schema) {
         User newUser = userMapper.mapUserRegistrationToEntity(schema);
         newUser = userRepository.save(newUser);
-        xmlUserRepository.save(newUser);
+
         return userMapper.mapEntityToUserRead(newUser);
     }
 
@@ -45,6 +42,5 @@ public class UserService {
     public void delete(Long id) {
         User user = userRepository.findById(id).orElseThrow(NotFoundException::new);
         userRepository.delete(user);
-        xmlUserRepository.delete(user);
     }
 }
